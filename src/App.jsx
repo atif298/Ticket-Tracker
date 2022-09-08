2c48732a76e94a768ddf1c12f0b1564d63835cd2
atif298 <atifuddin03@gmail.com>
2022-09-08 11:34:48 +0100

    App.jsx created and EmployeCard inserted into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.scss';
+import EmployeCard from './components/EmployeCard/EmployeCard';
 
-function App() {
+import team from "./assets/data/team.js";
+
+const App = () => {
+  
   return (
     <div className="app">
-      
+      <header>
+        <h1 className='app__heading'>Ticket Tracker</h1>
+      </header>
+      <EmployeCard teamArr={team} />
     </div>
   );
 }
